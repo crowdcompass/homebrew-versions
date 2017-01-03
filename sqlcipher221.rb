d18de29e7bbaef870e3cb5e5bad18345ad6c4b85
Drew Vogel <dvogel@crowdcompass.com>
2017-01-03 12:41:00 -0600

    Updated sqlcipher recipe for homebrew 1.0+

--- sqlcipher221.rb
+++ sqlcipher221.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Sqlcipher221 < Formula
   homepage "http://sqlcipher.net"
   url 'https://github.com/sqlcipher/sqlcipher/archive/v2.2.1.tar.gz'
-  sha1 'dc8af219f71ddae63ed7caf9cff5c0e9b00c3f13'
+  sha256 '5432585796b9d83aaf2c3016c173ab17cec07337cb65afae74edf952dfb4aa92'
 
   head "https://github.com/sqlcipher/sqlcipher.git", :tag => 'v2.2.1'
 
@@ -11,7 +11,7 @@ class Sqlcipher221 < Formula
 
   def install
     system "./configure", "--prefix=#{prefix}", "--enable-tempstore=yes",
-                          "CFLAGS=-DSQLITE_HAS_CODEC", "LDFLAGS=-lcrypto",
+                          "CFLAGS=-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib", "LDFLAGS=-lcrypto",
                           "--disable-tcl"
     system "make"
     system "make install"
